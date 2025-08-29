4c42fab86b6a65e1587ecece72d9b5cab55c5343
retosugua <renanaugusto.ras@gmail.com>
2025-08-29 17:36:50 -0300

    trabalhdno com signals

--- anybank/src/app/app.component.ts
+++ anybank/src/app/app.component.ts
@@ -1,7 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, Signal, signal } from '@angular/core';
 import { BannerComponent } from "./banner/banner.component";
 import { FormNovaTransacaoComponent } from "./form-nova-transacao/form-nova-transacao.component";
-import { Transacao } from './modelos/transacao';
+import { TipoTransacao, Transacao } from './modelos/transacao';
 
 @Component({
   selector: 'app-root',
@@ -12,8 +12,29 @@ import { Transacao } from './modelos/transacao';
 export class AppComponent {
   transacoes = signal<Transacao[]>([]);
 
+  saldo = computed(() => {
+    return this.transacoes().reduce((acc: number, transacao: Transacao) => {
+      switch (transacao.tipo) {
+        case TipoTransacao.DEPOSITO:
+
+          return acc + transacao.valor
+
+        case TipoTransacao.SAQUE:
+          return acc - transacao.valor
+
+        default:
+          throw new Error("Tipo de transação inválida");
+      }
+    }, 0)
+  })
+
+
+
   processarTransacao(transacao: Transacao) {
-    console.log(transacao);
+    this.transacoes.update(listaAtual => [transacao, ...listaAtual])
+
+    console.log(this.transacoes());
+
 
   }
 }
